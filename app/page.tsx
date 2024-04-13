0b25e7873db9b11919c66dacb80b1540e2fba31d
amirsep <amir.sep19@gmail.com>
2024-04-14 00:01:36 +0530

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <>
-      <p className="text-green-500 font-bold">Hello Lingo!</p>
-    </>
+    <div>
+      <Button size="lg">Click Me</Button>
+    </div>
   );
 }
